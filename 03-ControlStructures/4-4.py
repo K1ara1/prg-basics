292a7f70bf960f20879cf644028728b615e0f767
Klara Filipowska <klarafilipowska@gmail.com>
2024-10-15 11:47:57 +0200

    tasks part 2

---
+++ 03-ControlStructures/4-4.py
@@ -0,0 +1,8 @@
+university = 'Krakow University of Economics'
+university_expanded = ''
+
+for char in university:
+    univerity_expanded = university_expanded + ... + ...
+
+print(...)
+print(...)
